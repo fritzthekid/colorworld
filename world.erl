3115ff784d65277701ba3e3c6d66c07a58f631b5
Eduard Moser <eduard.moser@gmx.de>
2023-11-27 19:07:24 +0100

    world with 3 neigbors +oz (ju no kr)

--- world.erl
+++ world.erl
@@ -2,18 +2,17 @@
 -export([countries/0, neighbors/0]).
 
 countries() ->
-    [de,fr,sp,kr,pr,it,sb,gr,hu,sl,oe,tc,po,dk,nl,bl,lx,ch,oz].
+    [de,fr,sp,pr,it,ju,gr,hu,sl,oe,tc,po,dk,nl,bl,lx,ch,oz].
 
 neighbors() ->
     [{de,[dk,nl,bl,lx,fr,ch,oe,tc,po,oz]},
      {fr,[de,bl,sp,it,ch,lx,oz]},
      {sp,[fr,pr,oz]},
      {pr,[sp,oz]},
-     {it,[fr,ch,oe,kr,hu,oz]},
-     {sb,[kr,hu,gr,oz]},
-     {kr,[it,sb,oz]},
-     {gr,[sb,oz]},
-     {hu,[oe,it,sl,sb,oz]},
+     {it,[fr,ch,oe,ju,hu,oz]},
+     {ju,[it,hu,gr,oz]},
+     {gr,[ju,oz]},
+     {hu,[oe,it,sl,ju,oz]},
      {sl,[hu,tc,po,oe,oz]},
      {oe,[de,ch,it,hu,tc,sl]},
      {tc,[de,oe,sl,po]},
@@ -23,6 +22,6 @@ neighbors() ->
      {bl,[de,nl,fr,lx,oz]},
      {lx,[de,bl,fr]},
      {ch,[de,fr,it,oe]},
-     {oz,[de,nl,bl,fr,sp,po,it,sb,gr,hu,sl,pr,dk,kr]}
+     {oz,[de,nl,bl,fr,sp,po,it,ju,gr,hu,sl,pr,dk]}
     ].
 
