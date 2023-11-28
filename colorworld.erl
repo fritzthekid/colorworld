d1050e7fd1c5c37107bd0f61a17c8a5c4434d101
Eduard Moser <eduard.moser@gmx.de>
2023-11-28 18:19:16 +0100

    new approach based on raw edge list

--- colorworld.erl
+++ colorworld.erl
@@ -1,12 +1,15 @@
+%% color some graph, if planar should be not more than 4 colors
+%% color_world/0 colors only europe (from the graph in world.erl)
+%% color_world/1 could be color_world(gengraph:gengraph(150,20)) - just random graph (not planar)
 -module(colorworld).
 -export([color_world/0, color_world/1]).
 
-check_neighbors(D) ->
-    C = dict:fetch_keys(D),
+check_neighbors(WL) ->
+    C = countries(WL),
     lists:foldr(fun(CC, Acc) ->
-		      L = dict:fetch(CC,D),
+		      L = neighbors(CC,WL),
 		      Acc + lists:foldr(fun(X,AccI) ->
-						Len = length(lists:filter(fun(XX) -> XX =:= CC end, dict:fetch(X,D))),
+						Len = length(lists:filter(fun(XX) -> XX =:= CC end, neighbors(X,WL))),
 						if Len > 0 -> 
 							AccI;
 						   true ->
@@ -16,24 +19,26 @@ check_neighbors(D) ->
 					end,
 					0, L) 
 		end, 0, C).
+
+%%% color_world assign color to every vertice (countries)
+
 color_world() ->
-    color_world(neighbor_dict()).
-color_world(D) ->
-    N = check_neighbors(D),
+    color_world(world:world_graph()).
+color_world(WL) ->
+    N = check_neighbors(WL),
     if N =/= 0 ->
 	    throw({"check_neighbors() failed.\n"});
        true ->
 	    ok
     end,
-    NDL = lists:sort(fun(A,B) -> length(element(2, A)) > length(element(2,B)) end,dict:to_list(D)),
-    CC = element(1, hd(NDL)),
-    color_world([{CC,0}],tl(NDL),D).
-color_world(L,[],_) -> 
-    {L, length(L), lists:foldr(fun(E,Acc) -> max(Acc,element(2,E)) end, 0, L)};
-color_world(L,[H|T],D) ->
-    CL = dict:fetch_keys(D),
-    NC = firstnotusedcolor(L,neighbors(element(1,H),D),length(CL)),
-    color_world(L++[{element(1,H),NC}],T,D).
+    [{CC,_}|TL] = lists:sort(fun(A,B) -> length(element(2, A)) > length(element(2,B)) end,neighbor_struct(WL)),
+    color_world([{CC,1}],TL,WL).
+color_world(ColL,[],_) -> 
+    {ColL, length(ColL), lists:foldr(fun(E,Acc) -> max(Acc,element(2,E)) end, 0, ColL)};
+color_world(ColL,[H|T],WL) ->
+    CL = countries(WL),
+    NC = firstnotusedcolor(ColL,neighbors(element(1,H),WL),length(CL)),
+    color_world(ColL++[{element(1,H),NC}],T,WL).
 
 firstnotusedcolor(L,Neighbors,N) ->
     ColorD = dict:from_list(L),
@@ -41,8 +46,22 @@ firstnotusedcolor(L,Neighbors,N) ->
     ColorsUsed = lists:foldr(fun(C, Acc) -> [dict:fetch(C,ColorD)]++Acc end, [], ColoredNeighbors),
     lists:min(lists:filter(fun(C) -> lists:member(C,ColorsUsed) =:= false end, lists:seq(1,N))).
 	      
-neighbor_dict() ->
-    dict:from_list(world:neighbors()).
+neighbors(C,L) ->
+    lists:foldr(fun(E,Acc) ->
+			 if 
+			     element(1,E) =:= C ->
+				 Acc ++ [element(2,E)];
+			     element(2,E) =:= C ->
+				 Acc ++ [element(1,E)];
+			     true ->
+				 Acc
+			 end
+		end, [], L).
 
-neighbors(C,D) ->
-    dict:fetch(C,D).
+countries(L) ->
+    R = lists:foldr(fun(E,Acc) ->  Acc ++ [element(1,E), element(2,E)] end, [], L),
+    sets:to_list(sets:from_list(R)).
+
+neighbor_struct(WL) ->
+    lists:foldr(fun(C, Acc) -> Acc ++ [{C,neighbors(C,WL)}] end,[],countries(WL)).
+
