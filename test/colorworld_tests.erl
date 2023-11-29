91192a78ce46a295096f26853472236a99463bd6
Eduard Moser <eduard.moser@gmx.de>
2023-11-29 22:41:39 +0100

    add internal tests and test coverage

--- test/colorworld_tests.erl
+++ test/colorworld_tests.erl
@@ -7,11 +7,24 @@ coloreurope_test() ->
     ?assertEqual(19,Count),
     ?assertEqual(4,Colors).
 
+%% see neighbors should be [dk,tc,nl,po,fr,oe,lx,ch,bl]
+neighbor_test() ->
+    N = colorworld:neighbors(de,world:world_graph()),
+    ?assert(is_list(N),"should be list"),
+    Expect = [dk,tc,nl,po,fr,oe,lx,ch,bl],
+    lists:filter(fun(C) -> ?assert(lists:member(C,N)),true end, Expect).
+
+contry_test() ->
+    CL = colorworld:countries(world:world_graph()),
+    Expect = [sp,v0,pr,gr,fr,tc,it,oe,ju,hu,de,bw,lx,ch,dk,sl,nl,bl,po],
+    ?assert(is_list(CL), "countries is list."),
+    ?assertEqual(length(Expect),length(CL), "identical length."),
+    lists:filter(fun(C) -> ?assert(lists:member(C,CL)),true end, Expect).
+
 gengraph_test() ->
     {CL,Count,_} = colorworld:color_world(gengraph:gen_graph(150,20)),
     ?assert(is_list(CL),"Colortable, is no a list."),
     ?assert(Count>15,"Only less then 15 vertices").
-
 
 example_graph_test() ->
     {_,_,Exa1} = colorworld:color_world(planar:nonplanargraph()),
