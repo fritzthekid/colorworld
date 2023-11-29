b988ba2b2108e941038ea494ffbaf06156ddaf92
Eduard Moser <eduard.moser@gmx.de>
2023-11-29 21:36:58 +0100

    color non planar graphs

--- test/colorworld_tests.erl
+++ test/colorworld_tests.erl
@@ -2,27 +2,25 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-colorworld_test() ->
-    ok.
-
 coloreurope_test() ->
     {_,Count,Colors} = colorworld:color_world(),
-    if 
-	(Count =/= 19) ->
-	    throw({"number of countries do not match ww instead of 19."});
-	(Colors =/= 4) ->
-	    throw({"number of colors do not match ww instead of 4."});
-	true ->
-	    ok
-    end.
+    ?assertEqual(19,Count),
+    ?assertEqual(4,Colors).
 
 gengraph_test() ->
     {CL,Count,_} = colorworld:color_world(gengraph:gen_graph(150,20)),
-    if 
-	is_list(CL) =/= true ->
-	    throw({"Colortable, is no list."});
-	Count < 15 ->
-	    throw({"Only less then 15 vertices"});
-	true ->
-	    ok
-    end.
+    ?assert(is_list(CL),"Colortable, is no a list."),
+    ?assert(Count>15,"Only less then 15 vertices").
+
+
+example_graph_test() ->
+    {_,_,Exa1} = colorworld:color_world(planar:nonplanargraph()),
+    ?assertEqual(3,Exa1,"planar:nonplanargraph"),
+    {_,_,Exa2} = colorworld:color_world(planar:fourcolorsgraph()),
+    ?assertEqual(4,Exa2,"planar:fourcolorsgraph"),
+    {_,_,Exa3} = colorworld:color_world(planar:simple_fivecolor()),
+    ?assertEqual(5,Exa3,"planar:simple_fivecolor"),
+    {_,_,Exa4} = colorworld:color_world(planar:bipartite_graph()),
+    ?assertEqual(3,Exa4,"planar:bipartite_graph"),
+    {_,_,Exa5} = colorworld:color_world(planar:dice()),
+    ?assertEqual(4,Exa5,"planar:dice").
